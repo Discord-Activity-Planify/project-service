d3d5777e22b558ae344fa683131dd43cf6de9134
kittipong-dev <achikrab@gmail.com>
2025-01-13 01:56:24 +0700

    update invitationsApi.ts to merge route parameters in the router

--- src/routers/invitationsApi.ts
+++ src/routers/invitationsApi.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { invite } from "../controllers/invitationsController";
 
-const router = express.Router()
+const router = express.Router({mergeParams: true})
 
 router.post("/", invite)
 
