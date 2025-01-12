0ddfb36dbc70919be5d613177ff507e9fe648850
kittipong-dev <achikrab@gmail.com>
2025-01-13 01:39:46 +0700

    create membersController.ts and update memberApi.ts

--- src/routers/membersApi.ts
+++ src/routers/membersApi.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { getMembers } from "../controllers/membersController";
 
-const router = express.Router()
+const router = express.Router({mergeParams: true})
 
 router.get("/", getMembers)
 
