3d1087b85f19ace1a04e5760212129b8d3132f4f
kittipong-dev <achikrab@gmail.com>
2025-02-04 23:55:47 +0700

    feat: add Discord bot token to config and implement user data retrieval from Discord API

--- src/controllers/membersController.ts
+++ src/controllers/membersController.ts
@@ -1,19 +1,27 @@
 import { Request, Response } from 'express';
 import { AccessProject } from '../db/models/AccessProject';
+import { getUserDataFromDiscord } from './getUserDataFromDiscord';
 
 // /api/v1/projects/:projectId/members
 // get
 const getMembers = async (req: Request, res: Response) => {
     const { projectId } = req.params;
     if (projectId) {
-        console.log("inin")
         try {
             const data = await AccessProject.findAll({
                 where: { projectId: parseInt(projectId), isActive: true },
                 attributes: ['userId']
             });
+            const userDataPromises = data.map(async (item) => {
+                const userData = await getUserDataFromDiscord(item.toJSON().userId);
+                return {
+                    ...userData
+                };
+            });
 
-            res.json(data.map((item) => item.toJSON().userId));
+            const userData = await Promise.all(userDataPromises);
+
+            res.json(userData);
         } catch (error) {
             res.status(500).json({ error: 'Error fetching members' });
         }
